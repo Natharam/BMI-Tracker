2e6d6528573706653ed3e3590570bb6db81a653a
Natharam <201651033@iiitvadodara.ac.in>
2020-11-10 17:51:46 +0530

    first commit

---
+++ build/precache-manifest.34027d367898db8d263a064d1359f5fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecef7dbceaf0c4d8bffe7cb9403df723",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "8293a912b58224040af6",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "20f0a58b7748648c1f17",
+    "url": "/bmi-calculator/static/css/main.160e4576.chunk.css"
+  },
+  {
+    "revision": "8293a912b58224040af6",
+    "url": "/bmi-calculator/static/js/2.920416a7.chunk.js"
+  },
+  {
+    "revision": "20f0a58b7748648c1f17",
+    "url": "/bmi-calculator/static/js/main.5e2808a4.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
